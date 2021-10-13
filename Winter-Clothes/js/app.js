d8c1af94dc40b31c43c0a17188db2929ac19af80
KurKuza <danilsherrbakov1993@gmail.com>
2021-10-13 16:27:13 +0300

    i doing slider

--- Winter-Clothes/js/app.js
+++ Winter-Clothes/js/app.js
@@ -790,24 +790,6 @@ animate({
 			Element.prototype.msMatchesSelector;
 	}
 })();
-
-// function parallax(event) {
-// 	this.querySelectorAll('.parallax_image').forEach(parallax_image => {
-// 		let speed = parallax_image.getAttribute('data-speed')
-// 		parallax_image.style.transform = `translatex(${event.clientX*speed/400}px)`
-// 	});
-
-// };
-
-// 	document.addEventListener('mousemove', parallax);
-
-
-
-
-
-
-
-
 "use strict"
 
 // Ждем загрузку контента
@@ -822,11 +804,11 @@ window.onload = function () {
 		const image5 = document.querySelector('.parallax-images__image-5');
 
 		// Коэффициенты
-		const forImage1 = 1;
-		const forImage2 = 1.1;
-		const forImage3 = 1.5;
-		const forImage4 = 1.25;
-		const forImage5 = 2;
+		const forImage1 = 1/1.3;
+		const forImage2 = 1.1/1.3;
+		const forImage3 = 1.5/1.3;
+		const forImage4 = 1.25/1.3;
+		const forImage5 = 2/1.3;
 
 		// Скорость анимации
 		const speed = 0.05;
@@ -881,14 +863,13 @@ window.onload = function () {
 		// 	threshold: thresholdSets
 		// });
 
-		// observer.observe(document.querySelector('.content'));
+		// observer.observe(document.querySelector('.advantages'));
 
 		// function setParallaxItemsStyle(scrollTopProcent) {
 		// 	content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
 		// 	mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
 		// 	human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
 		// }
-
 
 	}
 }
@@ -1858,24 +1839,6 @@ function scroll_animate(event) {
 	//disableScroll();
 }
 
-
-// function parallax(event) {
-// 	this.querySelectorAll('.parallax_image').forEach(parallax_image => {
-// 		let speed = parallax_image.getAttribute('data-speed')
-// 		parallax_image.style.transform = `translatex(${event.clientX*speed/400}px)`
-// 	});
-
-// };
-
-// 	document.addEventListener('mousemove', parallax);
-
-
-
-
-
-
-
-
 "use strict"
 
 // Ждем загрузку контента
@@ -1890,11 +1853,11 @@ window.onload = function () {
 		const image5 = document.querySelector('.parallax-images__image-5');
 
 		// Коэффициенты
-		const forImage1 = 1;
-		const forImage2 = 1.1;
-		const forImage3 = 1.5;
-		const forImage4 = 1.25;
-		const forImage5 = 2;
+		const forImage1 = 1/1.3;
+		const forImage2 = 1.1/1.3;
+		const forImage3 = 1.5/1.3;
+		const forImage4 = 1.25/1.3;
+		const forImage5 = 2/1.3;
 
 		// Скорость анимации
 		const speed = 0.05;
@@ -1949,7 +1912,7 @@ window.onload = function () {
 		// 	threshold: thresholdSets
 		// });
 
-		// observer.observe(document.querySelector('.content'));
+		// observer.observe(document.querySelector('.advantages'));
 
 		// function setParallaxItemsStyle(scrollTopProcent) {
 		// 	content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
@@ -1957,6 +1920,131 @@ window.onload = function () {
 		// 	human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
 		// }
 
-
 	}
 }
+//BildSlider
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+
+			if (slider.classList.contains('_swiper_scroll')) {
+				let sliderScroll = document.createElement('div');
+				sliderScroll.classList.add('swiper-scrollbar');
+				slider.appendChild(sliderScroll);
+			}
+		}
+		if (slider.classList.contains('_gallery')) {
+			//slider.data('lightGallery').destroy(true);
+		}
+	}
+	sliders_bild_callback();
+}
+
+function sliders_bild_callback(params) { }
+
+let sliderScrollItems = document.querySelectorAll('._swiper_scroll');
+if (sliderScrollItems.length > 0) {
+	for (let index = 0; index < sliderScrollItems.length; index++) {
+		const sliderScrollItem = sliderScrollItems[index];
+		const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
+		const sliderScroll = new Swiper(sliderScrollItem, {
+			observer: true,
+			observeParents: true,
+			direction: 'vertical',
+			slidesPerView: 'auto',
+			freeMode: true,
+			scrollbar: {
+				el: sliderScrollBar,
+				draggable: true,
+				snapOnRelease: false
+			},
+			mousewheel: {
+				releaseOnEdges: true,
+			},
+		});
+		sliderScroll.scrollbar.updateSize();
+	}
+}
+
+
+function sliders_bild_callback(params) { }
+
+let slider_about = new Swiper('.cards__slider', {
+	/*
+	grid: {
+	rows: 2,
+},
+	effect: 'fade',
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	*/
+	observer: true, 
+	observeParents: true,
+	slidesPerView: 3.05,
+	spaceBetween: 50,
+	autoHeight: true,
+	speed: 800,
+	//touchRatio: 0,
+	//simulateTouch: false,
+	//loop: true,
+	//preloadImages: false,
+	//lazy: true,
+	// Dotts
+	//pagination: {
+	//	el: '.slider-quality__pagging',
+	//	clickable: true,
+	//},
+	// Arrows
+	navigation: {
+		nextEl: '.about__more .cards__navigation_next',
+		prevEl: '.about__more .cards__navigation_next',
+	},
+	/*
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		1268: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+		},
+	},
+	*/
+	on: {
+		lazyImageReady: function () {
+			ibg();
+		},
+	}
+	// And if we need scrollbar
+	//scrollbar: {
+	//	el: '.swiper-scrollbar',
+	//},
+});
